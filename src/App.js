c20855883c36ee7f83118dee2a6428348a49b53a
sandeep samanth <sandeepsamanth84@gmail.com>
2022-04-07 15:48:46 +0530

    ..

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import Header from './Header';
+import AddItem from './AddItem'
+import Content from './Content';
+import Footer from './Footer';
+import { useState } from "react";
+import SearchItem from './SearchItem';
+
+function App() {
+   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')))
+
+  const [newItem,setNewItem]=useState('')
+  const [search,setSearch]=useState('')
+    
+    const setAndSaveItems=(newItems)=>{
+        setItems(newItems)
+        localStorage.setItem('shoppinglist',JSON.stringify(newItems))
+    }
+
+    const addItem=(item)=>{
+      const id=items.length? items[items.length-1].id+1:1;
+      const myNewItem={id,checked:false,item}
+      const listItems=[...items,myNewItem]
+      setAndSaveItems(listItems)
+    }
+
+    const handleCheck=(id)=>{
+        const listItems=items.map((item)=>item.id===id?{...item,checked:!item.checked }:item);
+        // console.log(listItems)
+        setAndSaveItems(listItems)
+    }
+
+    const handleDelete=(id)=>{
+        const listItems =items.filter((item)=>item.id!==id)
+        setAndSaveItems(listItems)
+
+    }
+
+    const handleSubmit=(e)=>{
+      //stop page reload
+      e.preventDefault();
+      if(!newItem) return;
+      //add item 
+      addItem(newItem)
+      setNewItem('')
+    }
+
+  return (
+    <div className="App">
+      <Header title="Groceries List" />
+      
+      <AddItem 
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
+      <Content
+         items={items.filter(item=>((item.item).toLowerCase()).includes(search.toLowerCase()))}
+         handleCheck={handleCheck}
+         handleDelete={handleDelete}
+       />
+      <Footer length={items.length}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LineItem.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {FaTrashAlt} from 'react-icons/fa'
+
+
+
+const LIneItem = ({item,handleCheck,handleDelete}) => {
+  return (
+    <li className="item" key={item.id}>
+                    <input 
+                        type="checkbox"
+                        onChange={()=>handleCheck(item.id)}
+                        checked={item.checked}
+                    />
+
+                    <label 
+                        style={(item.checked)?{textDecoration:'line-through'}:null}
+                        onDoubleClick={()=>handleCheck(item.id)}
+                    >{item.item}</label>
+
+                    <FaTrashAlt 
+                        role='button' 
+                        tabIndex="0" 
+                        aria-label={`Delete ${item.item}`}
+                        onClick={()=>handleDelete(item.id)}
+
+                    />
+                    </li>)
+  
+}
+
+export default LIneItem
